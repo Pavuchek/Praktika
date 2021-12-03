264a29af83e773ce49d59806e12e174da944089b
LilChihba <danila-yurov@mail.ru>
2021-12-03 13:01:44 +0300

    Выполнил задание 4.1

--- WinBD/Form1.cs
+++ WinBD/Form1.cs
@@ -16,5 +16,20 @@ namespace WinBD
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Data.OleDb.OleDbDataReader myReader;
+            string PostavkiString;
+            oleDbConnection1.Open();
+            myReader = oleDbCommand1.ExecuteReader();
+            while(myReader.Read())
+            {
+                PostavkiString = myReader[1].ToString() + " " + myReader[2].ToString();
+                listBox1.Items.Add(PostavkiString);
+            }
+            myReader.Close();
+            oleDbConnection1.Close();
+        }
     }
 }
